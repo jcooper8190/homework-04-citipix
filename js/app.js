33d6b3bc3d1738a51dc1cef48c9cb76e62844120
jcoope8190 <joshua.cooper@wework.com>
2016-06-18 13:26:11 -0400

    HW4 Submission by Josh Cooper

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+// print background based on text entered.
+// text must be the name of a city.
+// city name has a class matching that city name in css. 
+// reset background with new submission.
+ 
+// listen for user clicking 'submit'/submitting text
+$("#submit-btn").click(changeBackground);
+ 
+function changeBackground(event) {
+ 
+  event.preventDefault();
+ 
+  //text entry
+  var city = $("#city-type").val();
+  
+  //match entry with proper css class (name of city)
+  
+  if(city === "NYC" || city === "new york city" || city === "new york" || city === "New York" || city === "New York City" || city === "nyc"){
+      $("body").attr("class", "nyc");
+  }
+  
+  else if(city === "SF" || city === "san francisco" || city === "bay area" || city === "Bay Area" || city === "San Francisco" || city === "sf"){
+      $("body").attr("class", "sf");
+  }
+  
+  else if(city === "LA" || city === "los angeles" || city === "LAX" || city === "la" || city === "Los Angeles"){
+      $("body").attr("class", "la");
+  }
+  
+  else if(city === "ATX" || city === "austin" || city === "Austin" || city === "atx"){
+      $("body").attr("class", "austin");
+  }
+  
+  else if(city === "sydney" || city === "SYD" || city === "Sydney" || city === "syd"){
+      $("body").attr("class", "sydney");
+  }
+ 
+ 
+  }
+}); 
